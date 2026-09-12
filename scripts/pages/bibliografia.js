4f4bd4126f8106b0fd7b43ffa8e5c6dabd565f11
Pedro Nuñez Yepiz <pedro_yepiz@uabc.edu.mx>
2026-09-12 05:04:55 -0700

    Add files via upload

--- scripts/pages/bibliografia.js
+++ scripts/pages/bibliografia.js
@@ -1,37 +1,6 @@
 (() => {
   "use strict";
 
-  /*
-   * BIBLIOGRAFÍA EN GOOGLE DRIVE
-   * Cada tarjeta del HTML contiene data-drive-id con el ID del PDF.
-   */
-  const DRIVE_FOLDER =
-    "https://drive.google.com/drive/folders/1FJu34OcEleYsAF8YvTG1nv-RVDH1cnOf?usp=drive_link";
-
-  function viewUrl(fileId) {
-    return `https://drive.google.com/file/d/${fileId}/view?usp=drive_link`;
-  }
-
-  function downloadUrl(fileId) {
-    return `https://drive.google.com/uc?export=download&id=${fileId}`;
-  }
-
-  function configurarEnlaces() {
-    document.querySelectorAll("[data-book]").forEach((card) => {
-      const fileId = card.dataset.driveId;
-      if (!fileId) return;
-
-      const view = card.querySelector('[data-action="view"]');
-      const download = card.querySelector('[data-action="download"]');
-
-      if (view) view.href = viewUrl(fileId);
-      if (download) download.href = downloadUrl(fileId);
-    });
-
-    const repo = document.querySelector(".repo-btn");
-    if (repo) repo.href = DRIVE_FOLDER;
-  }
-
   function configurarBusqueda() {
     const input = document.getElementById("bibliografiaSearch");
     const count = document.getElementById("bibliografiaCount");
@@ -62,8 +31,5 @@
     filtrar();
   }
 
-  document.addEventListener("DOMContentLoaded", () => {
-    configurarEnlaces();
-    configurarBusqueda();
-  });
+  document.addEventListener("DOMContentLoaded", configurarBusqueda);
 })();
